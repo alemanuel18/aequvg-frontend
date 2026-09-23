41fabdddc10434cd27757743e85b7cce6e496b48
alemanuel <alemanuelj5@gmail.com>
2026-09-23 08:17:23 -0600

    icons

---
+++ src/components/common/AppIcon.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+defineProps<{
+  name: 'arrow-up-right' | 'facebook' | 'instagram' | 'link' | 'mail' | 'map-pin' | 'phone' | 'send'
+  size?: number
+}>()
+</script>
+
+<template>
+  <svg
+    class="app-icon"
+    :width="size ?? 20"
+    :height="size ?? 20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="1.9"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    aria-hidden="true"
+  >
+    <template v-if="name === 'mail'">
+      <rect x="3" y="5" width="18" height="14" rx="2" />
+      <path d="m3 7 9 6 9-6" />
+    </template>
+    <template v-else-if="name === 'phone'">
+      <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6A19.79 19.79 0 0 1 2.12 4.18 2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72c.12.9.33 1.78.62 2.63a2 2 0 0 1-.45 2.11L8 9.73a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.85.29 1.73.5 2.63.62A2 2 0 0 1 22 16.92Z" />
+    </template>
+    <template v-else-if="name === 'map-pin'">
+      <path d="M20 10c0 5-8 12-8 12S4 15 4 10a8 8 0 1 1 16 0Z" />
+      <circle cx="12" cy="10" r="2.5" />
+    </template>
+    <template v-else-if="name === 'instagram'">
+      <rect x="3" y="3" width="18" height="18" rx="5" />
+      <circle cx="12" cy="12" r="4" />
+      <circle cx="17.5" cy="6.5" r=".8" fill="currentColor" stroke="none" />
+    </template>
+    <template v-else-if="name === 'facebook'">
+      <path d="M14 21v-8h3l.5-4H14V7c0-1.2.4-2 2.2-2H18V1.5c-.8-.1-1.7-.2-2.5-.2-3.1 0-5.5 1.9-5.5 5.5V9H7v4h3v8" />
+    </template>
+    <template v-else-if="name === 'send'">
+      <path d="m22 2-7 20-4-9-9-4Z" />
+      <path d="M22 2 11 13" />
+    </template>
+    <template v-else-if="name === 'arrow-up-right'">
+      <path d="M7 17 17 7" />
+      <path d="M7 7h10v10" />
+    </template>
+    <template v-else>
+      <path d="M10 13a5 5 0 0 0 7.5.5l2-2a5 5 0 0 0-7-7l-1.1 1" />
+      <path d="M14 11a5 5 0 0 0-7.5-.5l-2 2a5 5 0 0 0 7 7l1.1-1" />
+    </template>
+  </svg>
+</template>
